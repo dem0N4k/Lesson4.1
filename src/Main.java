4cc5ecc37d845bd9375937b584b811af9a4c3c9f
dem0N4k <demON4k1993@gmail.com>
2023-08-04 02:00:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        long age = 10;
+        byte height = 80;
+        short catCount = 5;
+        int boyWeight = 70;
+        int gerlWeight = 50;
+
+        String b = "hello";
+        char one = 'r';
+        char two = 't';
+        int asd = 10;
+        double zcx = 12.5;
+
+        System.out.println(age + "\n" + height + "\n" + catCount + "\n" +
+                boyWeight + "\n" + gerlWeight + "\n" + b + "\t" + one +
+                "\t" + two + "\t" + asd + "\t" + zcx + "\n\n");
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(catCount);
+        System.out.println(boyWeight);
+        System.out.println(gerlWeight);
+        System.out.println(b);
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(asd);
+        System.out.println(zcx);
+
+
+    }
+}
